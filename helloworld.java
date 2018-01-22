7d27cc7b1e016657e6740ea417b8725d8396e704
liliaguirre91 <lili91@nmsu.edu>
2018-01-21 18:12:02 -0700

    Add .gitignore and helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //Prints "Hello, World" to terminal
+        System.out.println("Hello, World");
+    }
+}//end class
+
